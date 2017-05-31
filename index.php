85c2fb04e4f175dfdb7d8a4beec41caf1cbcb0b8
thibault <thibault.daboville@gmail.com>
2017-05-31 14:06:46 +0200

    nettoyage code

--- index.php
+++ index.php
@@ -7,125 +7,112 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-			<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-					</div>
-					<div class="collapse navbar-collapse">
-						<ul class="nav navbar-nav">
-							<li class= "dropdown">
-								<li class="dropdown-toggle"><a href="#titre-carte">Carte</a></li>
-							</li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Langues<span class="caret"></span></a>
-								<ul class="dropdown-menu">
-									<li><a href="#">L1</a></li>
-									<li><a href="#">L2</a></li>
-									<li><a href="#">L3</a></li>
-								</ul>
-							</li>
-							<li class="dropdown-toggle"><a href="#titre-culture">Culture/Evènements</a></li>
-							<li class="dropdown-toggle"><a href="#titre-initiatives">Initiatives locales</a></li>
-							<li class="dropdown-toggle"><a href="#titre-patrimoine">Patrimoine</a></li>
+	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<ul class="nav navbar-nav">
+			<li class="dropdown-toggle"><a href="#titre-carte">Carte</a></li>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Langues<span class="caret"></span></a>
+				<ul class="dropdown-menu">
+					<li><a href="#">L1</a></li>
+					<li><a href="#">L2</a></li>
+					<li><a href="#">L3</a></li>
+					<li><a href="#">L4</a></li>
+					<li><a href="#">L5</a></li>
+					<li><a href="#">L6</a></li>
+				</ul>
+			</li>
+			<li class="dropdown-toggle"><a href="#titre-culture">Culture/Evènements</a></li>
+			<li class="dropdown-toggle"><a href="#titre-initiatives">Initiatives locales</a></li>
+			<li class="dropdown-toggle"><a href="#titre-patrimoine">Patrimoine</a></li>
 
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-								<ul id="login-dp" class="dropdown-menu">
-									<li>
-										<div class="row">
-											<div class="col-md-12">
-												Login via
-												<div class="social-buttons">
-													<a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
-													<a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
-												</div>
-												or
-												<form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-													<div class="form-group">
-														<label class="sr-only" for="exampleInputEmail2">Email address</label>
-														<input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required="">
-													</div>
-													<div class="form-group">
-														<label class="sr-only" for="exampleInputPassword2">Password</label>
-														<input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required="">
-														<div class="help-block text-right"><a href="">Forget the password ?</a></div>
-													</div>
-													<div class="form-group">
-														<button type="submit" class="btn btn-primary btn-block">Sign in</button>
-													</div>
-													<div class="checkbox">
-														<label>
-															<input type="checkbox"> keep me logged-in
-														</label>
-													</div>
-												</form>
-											</div>
-											<div class="bottom text-center">
-												New here ? <a href="#"><b>Join Us</b></a>
-											</div>
-										</div>
-									</li>
-								</ul>
-							</li>
-							<form class="navbar-form navbar-left">
-								<div class="form-group">
-									<input type="text" class="form-control" placeholder="Search">
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+				<ul id="login-dp" class="dropdown-menu">
+					<li>
+						<div class="row">
+							<div class="col-md-12">
+								Login via
+								<div class="social-buttons">
+									<a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+									<a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
 								</div>
-								<button type="submit" class="btn btn-default">Submit</button>
-							</form>
-						</ul>
-					</div>
-			</div>
-
-			<div id="carousel" class="carousel slide background" data-ride="carousel">
-						<ol class="carousel-indicators">
-							<div class="logo">
-								<img src="images/copyleft.png" id="logo" alt="logo"/>
+								or
+								<form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+									<div class="form-group">
+										<label class="sr-only" for="exampleInputEmail2">Email address</label>
+										<input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required="">
+									</div>
+									<div class="form-group">
+										<label class="sr-only" for="exampleInputPassword2">Password</label>
+										<input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required="">
+										<div class="help-block text-right"><a href="">Forget the password ?</a></div>
+									</div>
+									<div class="form-group">
+										<button type="submit" class="btn btn-primary btn-block">Sign in</button>
+									</div>
+									<div class="checkbox">
+										<label>
+											<input type="checkbox"> keep me logged-in
+										</label>
+									</div>
+								</form>
 							</div>
-							<li data-target="#carousel" data-slide-to="0"></li>
-							<li data-target="#carousel" data-slide-to="1"></li>
-							<li data-target="#carousel" data-slide-to="2"></li>
-						</ol>
-						<!-- Slides -->
-						<div class="carousel-inner">
-							<!-- Page 1 -->
-							<div class="item active">
-								<div class="carousel-page">
-									<img src="images/landscape.jpg" class="img-responsive"  alt="image1"/>
-								</div>
-								<div class="carousel-caption">Page 1 de présentation
-								</div>
-							</div>
-							<!-- Page 2 -->
-							<div class="item">
-								<div class="carousel-page">
-									<img src="images/landscape.jpg" class="img-responsive" alt="image2"   />
-								</div>
-								<div class="carousel-caption">Page 2 de présentation
-								</div>
-							</div>
-							<!-- Page 3 -->
-							<div class="item">
-								<div class="carousel-page">
-									<img src="images/landscape.jpg" class="img-responsive" alt="image3" />
-								</div>
-								<div class="carousel-caption">Page 3 de présentation
-								</div>
+							<div class="bottom text-center">
+								New here ? <a href="#"><b>Join Us</b></a>
 							</div>
 						</div>
-						<!-- Contrôles -->
-						<a class="left carousel-control" href="#carousel" data-slide="prev">
-							<span class="icon-prev"></span>
-						</a>
-						<a class="right carousel-control" href="#carousel" data-slide="next">
-							<span class="icon-next"></span>
-						</a>
+					</li>
+				</ul>
+			</li>
+			<form class="navbar-form navbar-left">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">GO</button>
+			</form>
+		</ul>
+	</div>
 
+	<div id="carousel" class="carousel slide background" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<div class="logo">
+				<img src="images/copyleft.png" id="logo" alt="logo"/>
+			</div>
+			<li data-target="#carousel" data-slide-to="0"></li>
+			<li data-target="#carousel" data-slide-to="1"></li>
+			<li data-target="#carousel" data-slide-to="2"></li>
+		</ol>
+		<!-- Slides -->
+		<div class="carousel-inner">
+			<!-- Page 1 -->
+			<div class="item active">
+				<div class="carousel-page">
+					<img src="images/landscape.jpg" class="img-responsive"  alt="image1"/>
+				</div>
+				<div class="carousel-caption">1</div>
+			</div>
+			<!-- Page 2 -->
+			<div class="item">
+				<div class="carousel-page">
+					<img src="images/landscape.jpg" class="img-responsive" alt="image2"   />
+				</div>
+				<div class="carousel-caption">2</div>
+			</div>
+			<!-- Page 3 -->
+			<div class="item">
+				<div class="carousel-page">
+					<img src="images/landscape.jpg" class="img-responsive" alt="image3" />
+				</div>
+				<div class="carousel-caption">3</div>
+			</div>
+		</div>
+		<!-- Contrôles -->
+		<a class="left carousel-control" href="#carousel" data-slide="prev">
+			<span class="icon-prev"></span>
+		</a>
+		<a class="right carousel-control" href="#carousel" data-slide="next">
+			<span class="icon-next"></span>
+		</a>
 	</div>
 
 	<div class="content">
@@ -164,19 +151,8 @@
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 		</div>
 	</div>
-
-
-	<section>
-		<button id="btn_up">
-			<span  class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
-		</button>
-	</section>
-</div>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<!-- <script type="text/javascript" src="js/app.js"></script> -->
-
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!-- <script type="text/javascript" src="js/app.js"></script> -->
 </body>
 </html>
