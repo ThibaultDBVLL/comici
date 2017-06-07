3c5b42c3d7d665da01d8b9e61bddf7019c2db07d
thibault <thibault.daboville@gmail.com>
2017-06-07 09:41:39 +0200

    map à corriger

--- index.php
+++ index.php
@@ -4,54 +4,54 @@
 	<meta charset="UTF-8">
 	<title>Comici</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../map_comici/css/leaflet-beautify-marker-icon.css" type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+
 	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
+		<div class="logo">
+			<img src="./images/copyleft.png" id="logonav" alt="logo"/>
+		</div>
 		<ul class="nav navbar-nav">
-			<li class="dropdown-toggle"><a href="#titre-culture">Culture/Evènements</a></li>
-			<li class="dropdown-toggle"><a href="#titre-initiatives">Initiatives locales</a></li>
-			<li class="dropdown-toggle"><a href="#titre-patrimoine">Patrimoine</a></li>
-			<li class="dropdown-toggle"><a href="#">Login</a></li>
+			<li class="dropdown-toggle" id="axeculture"><a href="#titre-culture">Culture/Evènements</a></li>
+			<li class="dropdown-toggle" id="axeinitiatives"><a href="#titre-initiatives">Initiatives locales</a></li>
+			<li class="dropdown-toggle" id="axepatrimoine"><a href="#titre-patrimoine">Patrimoine</a></li>
 		</ul>
 	</div>
+	<ul class="flexcontainer">
+		<li>
+			<a href="#">[Connexion]</a>
+		</li>
+	</ul>
 
-	<!-- sidebar -->
+	<!-- sidebar gauche-->
 
 	<nav class='sidebar sidebar-menu-collapsed flexpos'>
-	 <ul>
-		 <li class='active'>
-			 <a class='expandable' href='#' title='Dashboard'>
-				 <span class='glyphicon glyphicon-search'></span>
-				 <span class='expanded-element'>Dashboard</span>
-			 </a>
-		 </li>
-		 <li>
-			 <a class='expandable' href='#' title='Settings'>
-				 <span class='glyphicon glyphicon-map-marker'></span>
-				 <span class='expanded-element'>Settings</span>
-			 </a>
-		 </li>
-		 <li>
-			 <a class='expandable' href='#' title='Account'>
-				 <span class='glyphicon glyphicon-flag'></span>
-				 <span class='expanded-element'>Account</span>
-			 </a>
-		 </li>
-		 <li>
-		 	<div class="emty"></div>
-		 </li>
-	 </ul>
- </nav>
+		<ul>
+			<li class='active'>
+				<a class='expandable' href='#' title='Recherche'>
+					<span class='glyphicon glyphicon-search'></span>
+				</a>
+			</li>
+			<li>
+				<a class='expandable' href='/map_comici/map.php' title='Carte'>
+					<span class='glyphicon glyphicon-map-marker'></span>
+				</a>
+			</li>
+			<li>
+				<a class='expandable' href='#' title='Langue'>
+					<span class='glyphicon glyphicon-flag'></span>
+				</a>
+			</li>
+			<li>
+				<div class="emty"></div>
+			</li>
+		</ul>
+	</nav>
 
+	<!-- fin sidebar gauche -->
 
-	<!-- fin sidebar -->
 
 	<div id="carousel" class="carousel slide background" data-ride="carousel">
 		<ol class="carousel-indicators">
@@ -98,27 +98,27 @@
 	<div class="content">
 		<h2 id="titre-carte">Carte</h2>
 		<div id="carte">
-			<a href="http://placeholder.com"><img src="http://via.placeholder.com/800x500" alt="placeholder"></a>
+			<?php include ('map_comici/map.php'); ?>
 		</div>
 		<h2 id="titre-culture">Culture</h2>
-			<div class="row">
-				<div class="container">
-					<div id="culture">
-						<div class="col-md-6 col-s-8">
-								<a href="http://placeholder.com"><img src="http://via.placeholder.com/450x200" alt="placeholder"></a>
-						</div>
-						<div class="container col-md-6 col-s-4">
-							Culture ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>
-							n voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</div>
+		<div class="row">
+			<div class="container">
+				<div id="culture">
+					<div class="col-md-6 col-s-8">
+						<a href="http://placeholder.com"><img src="http://via.placeholder.com/450x200" alt="placeholder"></a>
+					</div>
+					<div class="container col-md-6 col-s-4">
+						Culture ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>
+						n voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 					</div>
 				</div>
 			</div>
+		</div>
 		<h2 id="titre-initiatives">Initatives</h2>
 		<div class="row desktop" id="three">
 			<div id="initiatives">
 				<div class="container col-md-6">
-						<a href="http://placeholder.com"><img src="http://via.placeholder.com/450x200" alt="placeholder"></a>
+					<a href="http://placeholder.com"><img src="http://via.placeholder.com/450x200" alt="placeholder"></a>
 				</div>
 				<div class="container col-md-6">
 					Culture ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>
@@ -130,7 +130,7 @@
 		<div class="row desktop" id="four">
 			<div id="patrimoine">
 				<div class="container col-md-6">
-						<a href="http://placeholder.com"><img src="http://via.placeholder.com/450x200" alt="placeholder"></a>
+					<a href="http://placeholder.com"><img src="http://via.placeholder.com/450x200" alt="placeholder"></a>
 				</div>
 				<div class="container col-md-6">
 					Culture ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>
@@ -142,48 +142,50 @@
 	<!-- footer -->
 	<hr>
 	<div class="container footer">
-  <div class="row">
-	<div class="col-xs-3">
-	 <div class="foot-header">Liens utiles</div>
-	 <div>
-		 <ul>
-		 	<li>1</li>
-		 	<li>2</li>
-		 	<li>3</li>
-		 	<li>4</li>
-		 </ul>
-	 </div>
- </div>
-<div class="col-xs-3">
- <div class="foot-header"> Nos partenaires </div>
- <div>
-	<ul>
-	 <li>1</li>
-	 <li>2</li>
-	 <li>3</li>
-	 <li>4</li>
-	</ul>
- </div>
-</div>
-<div class="col-xs-3">
-	Nous contacter
-</div>
-<div class="col-xs-3">
-	<div class="foot-header"> Plan du site </div>
-  <div>
- 	<ul>
- 	 <li>Carte</li>
- 	 <li>Culture/évènements</li>
- 	 <li>Initatives locales</li>
- 	 <li>Patrimoine</li>
- 	</ul>
-  </div>
-</div>
-</div>
-</div>
+		<div class="row">
+			<div class="col-xs-3">
+				<div class="foot-header">Liens utiles</div>
+				<div>
+					<ul>
+						<li>1</li>
+						<li>2</li>
+						<li>3</li>
+						<li>4</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-xs-3">
+				<div class="foot-header"> Nos partenaires </div>
+				<div>
+					<ul>
+						<li>1</li>
+						<li>2</li>
+						<li>3</li>
+						<li>4</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-xs-3">
+				Nous contacter
+			</div>
+			<div class="col-xs-3">
+				<div class="foot-header"> Plan du site </div>
+				<div>
+					<ul>
+						<li>Carte</li>
+						<li>Culture/évènements</li>
+						<li>Initatives locales</li>
+						<li>Patrimoine</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 	<!-- fin footer -->
 	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script type='text/javascript' src="/map_comici/js/leaflet-beautify-marker-icon.js"></script>
+	<script type='text/javascript' src="../map_comici/js/leaflet-beautify-marker.js"></script>
 	<script type="text/javascript" src="js/app.js"></script>
 </body>
 </html>
---
+++ js/app.js
@@ -0,0 +1,28 @@
+(function ($) {
+  $(document).ready(function(){
+
+	// hide .navbar first
+	// $(".navbar").hide();
+
+	// fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+			if ($(this).scrollTop() > 660) {
+				$('.navbar').fadeIn();
+				$('#axeculture').fadeIn();
+				$('#axeinitiatives').fadeIn();
+				$('#axepatrimoine').fadeIn();
+			} else {
+				// $('.navbar').fadeOut();
+				$('#axeculture').fadeOut();
+				$('#axeinitiatives').fadeOut();
+				$('#axepatrimoine').fadeOut();
+			}
+		});
+
+
+	});
+
+});
+  }(jQuery));
